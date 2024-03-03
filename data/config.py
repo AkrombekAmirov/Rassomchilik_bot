cb19826ea7741e616ad4ef58b1962b63d1554684
Akrombek <akrombekamirov@gmail.com>
2024-03-03 20:06:32 +0500

    init commit

---
+++ data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = environ.get("BOT_TOKEN")  # Bot toekn
+ADMINS = environ.get("ADMINS")  # adminlar ro'yxati
+IP = environ.get("ip")  # Xosting ip manzili
