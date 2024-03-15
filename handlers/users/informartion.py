1c10b2c71e1277556f3e733ecf581632aa47e03c
Akrombek <akrombekamirov@gmail.com>
2024-03-15 13:38:08 +0500

    13:37 time 15.03.2024 day

--- handlers/users/informartion.py
+++ handlers/users/informartion.py
@@ -9,35 +9,35 @@ from states import *
 @dp.message_handler(content_types=types.ContentType.CONTACT)
 async def get_contact(message: types.Message, state: FSMContext):
     await state.update_data({"number": message.contact.phone_number})
-    await message.answer("Familiya, Ism va sharifingizni kiriting.\nLotin alifbosida yozing.")
+    await message.answer("Familiya, Ism va sharifingizni kiriting.")
     await Person.number.set()
 
 
 @dp.message_handler(state=Person.number)
 async def get_name(message: types.Message, state: FSMContext):
     await state.update_data({"name": message.text})
-    await message.answer("Siz bilan bog'lanish uchun telifon raqamingizni kiriting.")
+    await message.answer("Siz bilan bog'lanish uchun telefon raqamingizni kiriting.")
     await Person.next()
 
 
 @dp.message_handler(state=Person.name)
 async def get_contact(message: types.Message, state: FSMContext):
     await state.update_data({"set_number": message.text})
-    await message.answer("Rasmlar sonini kiriting. Raqamlarda kiriting.")
+    await message.answer("Tanlovda nechta rasm bilan ishtirok etasiz?")
     await Person.next()
 
 
 @dp.message_handler(state=Person.set_number)
 async def get_school_number(message: types.Message, state: FSMContext):
     await state.update_data({"image_number": message.text})
-    await message.answer("Ta'lim olayotgan maktabingiz sonini kiriting. Raqamlarda kiriting.")
+    await message.answer("Ta'lim olayotgan maktabingiz raqamini kiriting.")
     await Person.next()
 
 
 @dp.message_handler(state=Person.image_number)
 async def get_school_number(message: types.Message, state: FSMContext):
     await state.update_data({"school_number": message.text})
-    await message.answer("Yoshingizni kiriting. Raqamlarda kiriting.")
+    await message.answer("Necha yoshdasiz?")
     await Person.next()
 
 
@@ -47,9 +47,9 @@ async def get_number(message: types.Message, state: FSMContext):
     data = await state.get_data()
     print(data, '789')
     da = (f"Familiya, Ism va sharifingiz: {data.get('name')}\n"
-          f"Telifon raqamingiz: {data.get('set_number')}",
+          f"Telefon raqamingiz: {data.get('set_number')}",
           f"Rasmlar soni: {data.get('image_number')}\n",
-          f"Ta'lim olayotgan maktabingiz soni: {data.get('school_number')}\n",
+          f"Ta'lim olayotgan maktabingiz raqami: {data.get('school_number')}\n",
           f"Yoshingiz: {data.get('age')}",)
     await message.answer(
         text=f"Telifon raqamingiz: {data.get('set_number')}\nF.I.SH: {data.get('name')}\nRasmlar soni: {data.get('image_number')}\nTa'lim olayotgan maktabingiz soni: {data.get('school_number')}\nYoshingiz: {data.get('age')}",
@@ -62,7 +62,7 @@ async def get_response(call: types.CallbackQuery, state: FSMContext):
     await state.update_data({"status": call.data})
     data = await state.get_data()
     if data.get("status") == "1":
-        await call.message.answer("Ma'lumotingiz qabul qilindi. “Rangtasvir asarlari” nomli bolalar rasmlar tanlovida sizni kutamiz.")
+        await call.message.answer("Ma'lumotingiz qabul qilindi. “Rangtasvir asarlari” nomli bolalar rasmlar tanlovida ishtirok etayotganingizdan minnatdormiz!")
         await file_read(data=[[f"{data.get('name')}", f"{data.get('number')}", f"{call.from_user.username}",
                                f"{data.get('set_number')}", f"{data.get('image_number')}",
                                f"{data.get('school_number')}", f"{data.get('age')}"]])
